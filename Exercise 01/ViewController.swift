1f36b3225695398d5213538dae58dfc351bbed24
Ricardo <rizacateco@gmail.com>
2016-01-09 22:43:24 -0600

    Added files

--- Exercise 01/ViewController.swift
+++ Exercise 01/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Exercise 01
 //
-//  Created by Richo Zacateco on 28/12/15.
+//  Created by Ricardo Zacateco on 28/12/15.
 //  Copyright © 2015 Ricardo Zacateco. All rights reserved.
 //
 
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redfaceimg: UIImageView!
+    @IBOutlet weak var bluefaceimg: UIImageView!
+    
+    @IBOutlet weak var txthda: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideblue(sender: AnyObject) {
+        bluefaceimg.hidden = true
 
+    }
+
+    @IBAction func hidered(sender: AnyObject) {
+        redfaceimg.hidden = true
+    }
 }
 
---
+++ Mathops.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var var1:Int = 15
+var var2:Int = 5
+var var3:Int = var1 + var2
+
+var var4:Int = var1 * 3
+var var5:Int = var1 - var2
+var var6:Int = var1 / var2
+var r1:Int = 11; var r2:Int = 9; var r3:Int = 34;
+let cons1:Int = 10; let cons2:Int = 12; let cons3:Int = 23;
+var exp:Double
+var exp1:Int = 423
+var str:String = "Hello "
+var str1:String = "World"
+
+var str8:String = str + str1
+var someVar:String = str8
